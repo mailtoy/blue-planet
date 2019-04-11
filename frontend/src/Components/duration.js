2ab4b9600378c2fa39276571c1f809e2c79f98b4
mailtoy <mai_kms11@hotmail.com>
2019-04-11 18:01:28 +0700

    Update

---
+++ frontend/src/Components/duration.js
@@ -0,0 +1,126 @@
+import React, { Component } from 'react';
+import { Slider, Card, Carousel } from 'antd';
+import '../css/duration.css';
+
+const { Meta } = Card;
+
+const marks = {
+    0: {
+        style: {
+            color: '#fff',
+        },
+        label: <strong>'1 to 3 Days'</strong>,
+    },
+    25: {
+        style: {
+            color: '#fff',
+        },
+        label: <strong>'4 to 6 Days'</strong>,
+    },
+    50: {
+        style: {
+            color: '#fff',
+        },
+        label: <strong>'7 to 9 Days'</strong>,
+    },
+    75: {
+        style: {
+            color: '#fff',
+        },
+        label: <strong>'10 to 12 Days'</strong>,
+    },
+    100: {
+        style: {
+            color: '#fff',
+        },
+        label: <strong>More</strong>,
+    },
+};
+
+class Duration extends Component {
+    state = {
+        value: 1,
+    }
+
+    onChange = (e) => {
+        console.log('radio checked area', e.target.value);
+        this.setState({
+            value: e.target.value,
+        });
+    }
+
+    render() {
+        return (
+            <div style={{ marginLeft: '110px', marginRight: '110px' }}>
+                <h4>Duration</h4>
+
+                <Slider marks={marks} step={null} defaultValue={0} />
+
+
+                <Carousel effect="fade">
+                    <div><h3>
+                        <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '10px' }}>
+                            <Card
+                                hoverable
+                                style={{ width: 200, height: 190, marginRight: '10px' }}
+                                cover={<img alt="example" src="https://gaijinpot.scdn3.secure.raxcdn.com/app/uploads/sites/6/2016/11/nara-park.jpg" />}
+                            >
+                                <Meta
+                                    title="Nara, Japan"
+                                // description="www.instagram.com"
+                                />
+                            </Card>
+                            <Card
+                                hoverable
+                                style={{ width: 200, height: 190, marginRight: '10px' }}
+                                cover={<img alt="example" src="https://cdn.theculturetrip.com/wp-content/uploads/2015/11/Switzerland-%C2%A9-Boris-Stroujko-Shutterstock.jpg" />}
+                            >
+                                <Meta
+                                    title="Switzerland"
+                                // description="www.instagram.com"
+                                />
+                            </Card>
+                            <Card
+                                hoverable
+                                style={{ width: 200, height: 190, marginRight: '10px' }}
+                                cover={<img alt="example" src="http://gotravelaz.com/wp-content/uploads/images/Rothenburg_25634.jpg" />}
+                            >
+                                <Meta
+                                    title="Germany"
+                                // description="www.instagram.com"
+                                />
+                            </Card>
+                            <Card
+                                hoverable
+                                style={{ width: 200, height: 190, marginRight: '10px' }}
+                                cover={<img alt="example" src="https://s-ec.bstatic.com/images/hotel/max400/166/166379438.jpg" />}
+                            >
+                                <Meta
+                                    title="Finland"
+                                // description="www.instagram.com"
+                                />
+                            </Card>
+                            <Card
+                                hoverable
+                                style={{ width: 200, height: 190 }}
+                                cover={<img alt="example" src="https://s-ec.bstatic.com/images/hotel/max400/166/166379438.jpg" />}
+                            >
+                                <Meta
+                                    title="Finland"
+                                // description="www.instagram.com"
+                                />
+                            </Card>
+                        </div>
+                    </h3></div>
+                    <div><h3>2</h3></div>
+                    <div><h3>3</h3></div>
+                    <div><h3>4</h3></div>
+                </Carousel>,
+            </div >
+        );
+    }
+}
+
+export default Duration;
+
+
---
+++ frontend/src/Components/themes.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import palm from '../images/palm-trees.svg';
+import pray from '../images/pray.svg';
+import world from '../images/world.svg';
+import mountain from '../images/mountain.svg';
+
+class Themes extends Component {
+
+    onClick = () => {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <br />
+                <br />
+                Holiday THEMES
+                <br />
+                <br />
+                <div style={{ display: 'flex', width: '70vw', margin: 'auto' }}>
+                    <div style={{ margin: 'auto', border: '1px solid #DEDFEC', width: 180, height: 120, padding: '5px' }}><img src={world} alt="Adventrue" style={{ width: 86, height: 120, height: 80 }} /><br />Adventrue</div>
+                    <div style={{ margin: 'auto', border: '1px solid #DEDFEC', width: 180, height: 120, padding: '5px' }}><img src={palm} alt="Nature" style={{ width: 86, height: 120, height: 80 }} /><br />Water Activities</div>
+                    <div style={{ margin: 'auto', border: '1px solid #DEDFEC', width: 180, height: 120, padding: '5px' }}><img src={pray} alt="Religion" style={{ width: 86, height: 120, height: 80 }} /><br />Religion</div>
+                    <div style={{ margin: 'auto', border: '1px solid #DEDFEC', width: 180, height: 120, padding: '5px' }}><img src={mountain} alt="Mountain" style={{ width: 80, height: 80 }} /> <br />Mountain</div>
+                </div >
+                < br />
+                <br />
+                <br />
+                <br />
+            </div >
+        );
+    }
+}
+
+export default Themes;
+
+
