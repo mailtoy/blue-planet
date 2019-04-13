1968c20aba3179ef121ae44bdc87b436c7dc48fa
boranorben <zhotkp@gmail.com>
2019-04-13 21:06:26 +0700

    move authentication part and redux provider to index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,6 @@ import './App.css';
 import Search from './Components/home/search';
 import Explore from './Pages/Explore';
 
-// import redux things
-import { Provider } from 'react-redux';
-import store from './store';
-
 // import testing components
 import Register from './Components/register/register'
 import Login from './Components/login/login'
@@ -19,29 +15,9 @@ import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
 
-// Check for token to keep user logged in
-if (localStorage.jwtToken) {
-  // Set auth token header auth
-  const token = localStorage.jwtToken;
-  setAuthToken(token);
-  // Decode token and get user info and exp
-  const decoded = jwt_decode(token);
-  // Set user and isAuthenticated
-  store.dispatch(setCurrentUser(decoded));
-  // Check for expired token
-  const currentTime = Date.now() / 1000; // to get in milliseconds
-  if (decoded.exp < currentTime) {
-    // Logout user
-    store.dispatch(logoutUser());
-    // Redirect to login
-    window.location.href = "./login";
-  }
-}
-
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
         <Router>
           <Switch>
             <Route exact path="/" component={Search}/>
@@ -56,7 +32,6 @@ class App extends Component {
           </Switch>
 
         </Router>
-      </Provider>
     );
   }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,40 @@ import * as serviceWorker from './serviceWorker';
 import "antd/dist/antd.css";
 import 'semantic-ui-css/semantic.min.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import redux things
+import { Provider } from 'react-redux';
+import store from './store';
+
+// authenication part
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+    // Set auth token header auth
+    const token = localStorage.jwtToken;
+    setAuthToken(token);
+    // Decode token and get user info and exp
+    const decoded = jwt_decode(token);
+    // Set user and isAuthenticated
+    store.dispatch(setCurrentUser(decoded));
+    // Check for expired token
+    const currentTime = Date.now() / 1000; // to get in milliseconds
+    if (decoded.exp < currentTime) {
+        // Logout user
+        store.dispatch(logoutUser());
+        // Redirect to login
+        window.location.href = "./login";
+    }
+}
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
